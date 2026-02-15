ec865dcccefd0dd6d084216cd09e8772eda0685d
kk42yy <161680482+kk42yy@users.noreply.github.com>
2026-02-15 10:50:28 +0800

    KL Loss

--- DeMoSeg/training/loss/loss_scheduler.py
+++ DeMoSeg/training/loss/loss_scheduler.py
@@ -21,7 +21,7 @@ class PolyLRScheduler(_LRScheduler):
         for param_group in self.optimizer.param_groups:
             param_group['lr'] = new_lr
 
-class DeepSupervisionLoss(_Loss):
+class DeepSupervisionLoss_noKL(_Loss):
     def __init__(self, loss, weights=[1.]):
         super().__init__()
         self.weight_factors = tuple(weights)
@@ -43,3 +43,36 @@ class DeepSupervisionLoss(_Loss):
                 if weights[i] != 0.0:
                     l += weights[i] * self.loss(*inputs)
         return l
+
+class DeepSupervisionLoss(nn.Module):
+    def __init__(self, loss, weight_factors=None):
+        """
+        use this if you have several outputs and ground truth (both list of same len) and the loss should be computed
+        between them (x[0] and y[0], x[1] and y[1] etc)
+        :param loss:
+        :param weight_factors:
+        """
+        super(DeepSupervisionLoss, self).__init__()
+        self.weight_factors = weight_factors
+        self.loss = loss
+
+    def forward(self, x, y):
+        if self.training:
+            x, kl = x
+            
+        else:
+            kl = 0
+
+        assert isinstance(x, (tuple, list)), "x must be either tuple or list"
+        assert isinstance(y, (tuple, list)), "y must be either tuple or list"
+        if self.weight_factors is None:
+            weights = [1] * len(x)
+        else:
+            weights = self.weight_factors
+
+        l = weights[0] * self.loss(x[0], y[0])
+        for i in range(1, len(x)):
+            if weights[i] != 0:
+                l += weights[i] * self.loss(x[i], y[i])
+
+        return l + kl
