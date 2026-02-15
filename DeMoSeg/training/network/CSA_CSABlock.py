73cd4bbdcb3ec0c6fd352eb3c1811d5d2277503f
kk42yy <161680482+kk42yy@users.noreply.github.com>
2026-02-15 11:03:05 +0800

    KL Loss

--- DeMoSeg/training/network/CSA_CSABlock.py
+++ DeMoSeg/training/network/CSA_CSABlock.py
@@ -60,7 +60,7 @@ class CSADropoutNormNonlin(nn.Module):
         x = self.conv(x)
         return self.lrelu(self.instnorm(x))
     
-class SplitConvCSA(nn.Module):
+class SplitConvCSA_noKL(nn.Module):
     def __init__(self, num_features) -> None:
         super().__init__()
 
@@ -182,3 +182,215 @@ class SplitConvCSA(nn.Module):
                                 missnum)
                               ], dim=0)
         return newx
+    
+class SplitConvCSA(nn.Module):
+    def __init__(self, num_features) -> None:
+        super().__init__()
+        self.output_channels = num_features
+        self.num_features = num_features
+        self.t1_conv = ConvDropoutNormNonlin(1, num_features)
+        self.t1_csa = CSADropoutNormNonlin(num_features, sort_small_first=True)
+        self.t1ce_conv = ConvDropoutNormNonlin(1, num_features)
+        self.t1ce_csa = CSADropoutNormNonlin(num_features)
+        self.t2_conv = ConvDropoutNormNonlin(1, num_features)
+        self.t2_csa = CSADropoutNormNonlin(num_features)
+        self.t2flare_conv = ConvDropoutNormNonlin(1, num_features)
+        self.t2flare_csa = CSADropoutNormNonlin(num_features)
+        self.klloss = torch.nn.KLDivLoss()
+
+    def feature_chosen(self, t1___, t1ce_, t2___, flair, missing_index):
+        '''
+        t1 <-> t2, t1ce <-> flair
+        '''
+        if missing_index == 0:   # t1
+            batch1 = torch.cat([t1___[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t1___[:,self.num_features*1//4:self.num_features*2//4,...],
+                                t1___[:,self.num_features*2//4:self.num_features*3//4,...],
+                                t1___[:,self.num_features*3//4:self.num_features*4//4,...]], dim=1)
+        elif missing_index == 1: # t1ce
+            batch1 = torch.cat([t1ce_[:,self.num_features*1//4:self.num_features*2//4,...],
+                                t1ce_[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t1ce_[:,self.num_features*3//4:self.num_features*4//4,...],
+                                t1ce_[:,self.num_features*2//4:self.num_features*3//4,...]], dim=1)
+        elif missing_index == 2: # t2
+            batch1 = torch.cat([t2___[:,self.num_features*2//4:self.num_features*3//4,...],
+                                t2___[:,self.num_features*3//4:self.num_features*4//4,...],
+                                t2___[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t2___[:,self.num_features*1//4:self.num_features*2//4,...]], dim=1)
+        elif missing_index == 3: # flair
+            batch1 = torch.cat([flair[:,self.num_features*3//4:self.num_features*4//4,...],
+                                flair[:,self.num_features*2//4:self.num_features*3//4,...],
+                                flair[:,self.num_features*1//4:self.num_features*2//4,...],
+                                flair[:,self.num_features*0//4:self.num_features*1//4,...]], dim=1)
+        elif missing_index == 4: # t1 t1ce
+            batch1 = torch.cat([t1___[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t1ce_[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t1___[:,self.num_features*2//4:self.num_features*3//4,...],
+                                t1ce_[:,self.num_features*2//4:self.num_features*3//4,...]], dim=1)
+        elif missing_index == 5: # t1 t2
+            batch1 = torch.cat([t1___[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t1___[:,self.num_features*1//4:self.num_features*2//4,...],
+                                t2___[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t2___[:,self.num_features*1//4:self.num_features*2//4,...]], dim=1)
+        elif missing_index == 6: # t1 flair
+            batch1 = torch.cat([t1___[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t1___[:,self.num_features*1//4:self.num_features*2//4,...],
+                                flair[:,self.num_features*1//4:self.num_features*2//4,...],
+                                flair[:,self.num_features*0//4:self.num_features*1//4,...]], dim=1)
+        elif missing_index == 7: # t1ce t2
+            batch1 = torch.cat([t1ce_[:,self.num_features*1//4:self.num_features*2//4,...],
+                                t1ce_[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t2___[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t2___[:,self.num_features*1//4:self.num_features*2//4,...]], dim=1)
+        elif missing_index == 8: # t1ce flair
+            batch1 = torch.cat([t1ce_[:,self.num_features*1//4:self.num_features*2//4,...],
+                                t1ce_[:,self.num_features*0//4:self.num_features*1//4,...],
+                                flair[:,self.num_features*1//4:self.num_features*2//4,...],
+                                flair[:,self.num_features*0//4:self.num_features*1//4,...]], dim=1)
+        elif missing_index == 9: # t2 flair
+            batch1 = torch.cat([t2___[:,self.num_features*2//4:self.num_features*3//4,...],
+                                flair[:,self.num_features*2//4:self.num_features*3//4,...],
+                                t2___[:,self.num_features*0//4:self.num_features*1//4,...],
+                                flair[:,self.num_features*0//4:self.num_features*1//4,...]], dim=1)
+        elif missing_index == 10: # t1 t1ce t2
+            batch1 = torch.cat([t1___[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t1ce_[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t2___[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t2___[:,self.num_features*1//4:self.num_features*2//4,...]], dim=1)
+        elif missing_index == 11: # t1 t1ce flair
+            batch1 = torch.cat([t1___[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t1ce_[:,self.num_features*0//4:self.num_features*1//4,...],
+                                flair[:,self.num_features*1//4:self.num_features*2//4,...],
+                                flair[:,self.num_features*0//4:self.num_features*1//4,...]], dim=1)
+        elif missing_index == 12: # t1 t2 flair
+            batch1 = torch.cat([t1___[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t1___[:,self.num_features*1//4:self.num_features*2//4,...],
+                                t2___[:,self.num_features*0//4:self.num_features*1//4,...],
+                                flair[:,self.num_features*0//4:self.num_features*1//4,...]], dim=1)
+        elif missing_index == 13: # t1ce t2 flair
+            batch1 = torch.cat([t1ce_[:,self.num_features*1//4:self.num_features*2//4,...],
+                                t1ce_[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t2___[:,self.num_features*0//4:self.num_features*1//4,...],
+                                flair[:,self.num_features*0//4:self.num_features*1//4,...]], dim=1)
+        elif missing_index == 14: # t1 t1ce t2 flair
+            batch1 = torch.cat([t1___[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t1ce_[:,self.num_features*0//4:self.num_features*1//4,...],
+                                t2___[:,self.num_features*0//4:self.num_features*1//4,...],
+                                flair[:,self.num_features*0//4:self.num_features*1//4,...]], dim=1)
+        else:
+            raise IndexError
+        
+        return batch1
+
+    
+    def forward(self, x, missing_index):
+        t1, t1ce, t2, t2flare = torch.split(x, 1, dim=1)
+        # t1: torch.Tensor = self.t1_csa(self.t1_conv(t1))
+        # t1ce: torch.Tensor = self.t1ce_csa(self.t1ce_conv(t1ce))
+        # t2: torch.Tensor = self.t2_csa(self.t2_conv(t2))
+        # t2flare: torch.Tensor = self.t2flare_csa(self.t2flare_conv(t2flare))
+
+        t1_ = self.t1_conv(t1)
+        t1: torch.Tensor = self.t1_csa(t1_)
+
+        t1ce_ = self.t1ce_conv(t1ce)
+        t1ce: torch.Tensor = self.t1ce_csa(t1ce_)
+
+        t2_ = self.t2_conv(t2)
+        t2: torch.Tensor = self.t2_csa(t2_)
+
+        t2flare_ = self.t2flare_conv(t2flare)
+        t2flare: torch.Tensor = self.t2flare_csa(t2flare_)
+        
+        newx = self.feature_chosen(
+                t1[0,...].unsqueeze(0),
+                t1ce[0,...].unsqueeze(0), 
+                t2[0,...].unsqueeze(0), 
+                t2flare[0,...].unsqueeze(0),
+                missing_index[0]
+            )
+        for b, missnum in enumerate(missing_index[1:], start=1):
+            newx = torch.cat([newx, 
+                              self.feature_chosen(
+                                t1[b,...].unsqueeze(0),
+                                t1ce[b,...].unsqueeze(0), 
+                                t2[b,...].unsqueeze(0), 
+                                t2flare[b,...].unsqueeze(0),
+                                missnum)
+                              ], dim=0)
+        if self.training:
+            return newx, self.kl_loss_calculate(t1_, t1ce_, t2_, t2flare_, missing_index)
+        return newx
+
+    def kl_loss_calculate(self, t1: torch.Tensor, ce: torch.Tensor, t2: torch.Tensor, fl: torch.Tensor, missing_index):
+        klloss = self.missing_klloss(
+            t1[0].unsqueeze(0), 
+            ce[0].unsqueeze(0), 
+            t2[0].unsqueeze(0), 
+            fl[0].unsqueeze(0),
+              missing_index[0]
+        )
+        for b in range(1, len(missing_index)):
+            klloss += self.missing_klloss(
+                t1[b].unsqueeze(0), 
+                ce[b].unsqueeze(0), 
+                t2[b].unsqueeze(0), 
+                fl[b].unsqueeze(0), 
+                missing_index[b]
+            )
+        return klloss * 0.5
+
+    def missing_klloss(self, t1, ce, t2, fl, missing_index):
+        batch_klloss = 0
+        C1, C2, C3, C4 = [self.num_features * i // 4 for i in range(1,5)]
+
+        # t1: t1, ce, t2, fl
+        # ce: ce, t1, fl, t2
+        # t2: t2, fl, t1, ce
+        # fl: fl, t2, ce, t1
+
+        if missing_index <= 3: # t1, t1ce, t2, flair
+            pass        
+        elif missing_index == 4: # t1 t1ce
+            batch_klloss += self.kl_loss(t1[:,C1:C2], ce[:,:C1]) + self.kl_loss(ce[:,C1:C2], t1[:,:C1])
+        elif missing_index == 5: # t1 t2
+            batch_klloss += self.kl_loss(t1[:,C2:C3], t2[:,:C1]) + self.kl_loss(t2[:,C2:C3], t1[:,:C1])
+        elif missing_index == 6: # t1 flair
+            batch_klloss += self.kl_loss(t1[:,C3:C4], fl[:,:C1]) + self.kl_loss(fl[:,C3:C4], t1[:,:C1])
+        elif missing_index == 7: # t1ce t2
+            batch_klloss += self.kl_loss(ce[:,C3:C4], t2[:,:C1]) + self.kl_loss(t2[:,C3:C4], ce[:,:C1])
+        elif missing_index == 8: # t1ce flair
+            batch_klloss += self.kl_loss(ce[:,C2:C3], fl[:,:C1]) + self.kl_loss(fl[:,C2:C3], ce[:,:C1])
+        elif missing_index == 9: # t2 flair
+            batch_klloss += self.kl_loss(t2[:,C1:C2], fl[:,:C1]) + self.kl_loss(fl[:,C1:C2], t2[:,:C1])
+        elif missing_index == 10: # t1 t1ce t2
+            batch_klloss += self.kl_loss(t1[:,C1:C2], ce[:,:C1]) + self.kl_loss(t1[:,C2:C3], t2[:,:C1]) + \
+                            self.kl_loss(ce[:,C1:C2], t1[:,:C1]) + self.kl_loss(ce[:,C3:C4], t2[:,:C1]) + \
+                            self.kl_loss(t2[:,C2:C3], t1[:,:C1]) + self.kl_loss(t2[:,C3:C4], ce[:,:C1])
+        elif missing_index == 11: # t1 t1ce flair
+            batch_klloss += self.kl_loss(t1[:,C1:C2], ce[:,:C1]) + self.kl_loss(t1[:,C3:C4], fl[:,:C1]) + \
+                            self.kl_loss(ce[:,C1:C2], t1[:,:C1]) + self.kl_loss(ce[:,C2:C3], fl[:,:C1]) + \
+                            self.kl_loss(fl[:,C2:C3], ce[:,:C1]) + self.kl_loss(fl[:,C3:C4], t1[:,:C1])
+        elif missing_index == 12: # t1 t2 flair
+            batch_klloss += self.kl_loss(t1[:,C2:C3], t2[:,:C1]) + self.kl_loss(t1[:,C3:C4], fl[:,:C1]) + \
+                            self.kl_loss(t2[:,C1:C2], fl[:,:C1]) + self.kl_loss(t2[:,C2:C3], t1[:,:C1]) + \
+                            self.kl_loss(fl[:,C1:C2], t2[:,:C1]) + self.kl_loss(fl[:,C3:C4], t1[:,:C1])
+        elif missing_index == 13: # t1ce t2 flair
+            batch_klloss += self.kl_loss(ce[:,C2:C3], fl[:,:C1]) + self.kl_loss(ce[:,C3:C4], t2[:,:C1]) + \
+                            self.kl_loss(t2[:,C1:C2], fl[:,:C1]) + self.kl_loss(t2[:,C3:C4], ce[:,:C1]) + \
+                            self.kl_loss(fl[:,C1:C2], t2[:,:C1]) + self.kl_loss(fl[:,C2:C3], ce[:,:C1])
+        elif missing_index == 14: # t1 t1ce t2 flair
+            batch_klloss += self.kl_loss(t1[:,C1:C2], ce[:,:C1]) + self.kl_loss(t1[:,C2:C3], t2[:,:C1]) + self.kl_loss(t1[:,C3:C4], fl[:,:C1]) + \
+                            self.kl_loss(ce[:,C1:C2], t1[:,:C1]) + self.kl_loss(ce[:,C2:C3], fl[:,:C1]) + self.kl_loss(ce[:,C3:C4], t2[:,:C1]) + \
+                            self.kl_loss(t2[:,C1:C2], fl[:,:C1]) + self.kl_loss(t2[:,C2:C3], t1[:,:C1]) + self.kl_loss(t2[:,C3:C4], ce[:,:C1]) + \
+                            self.kl_loss(fl[:,C1:C2], t2[:,:C1]) + self.kl_loss(fl[:,C2:C3], ce[:,:C1]) + self.kl_loss(fl[:,C3:C4], t1[:,:C1])
+        else:
+            raise IndexError
+        return batch_klloss
+
+    def kl_loss(self, x, y):
+        # x, y: [1, C, D, H, W]
+        T = 1
+        x = torch.nn.functional.log_softmax(x / T, dim=1)
+        y = torch.nn.functional.softmax(y / T, dim=1)
+        return self.klloss(x, y) * T * T
